b0c7bb3332cbd09638087fbbb325c60106f9b24f
pluskid <pluskid@gmail.com>
2009-03-12 16:01:36 +0800

    minor modification

--- sprite/basic.py
+++ sprite/basic.py
@@ -8,12 +8,9 @@ class SpBase(Sprite):
         self.hp = float('inf')
     
     def hurt(self, damage, god):
-        orig_hp = self.hp
         self.hp -= damage
         if self.hp <= 0:
             god.create_explosion_animation(vec2d(self.rect.center),
                                            'explode-large')
             self.kill()
-        if self.hp < orig_hp:
-            pass #TODO: add effect here
 
